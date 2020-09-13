dc24cf89acf1f2f052a7491866c5b07de11a4fbe
Bhimashankar <bhimamalbhage@hotmail.com>
2020-09-13 23:17:47 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,118 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
-// here
+const isValidNum = (req, res, next) => {
+  const { num1, num2 } = req.body;
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  }
+  next();
+};
 
+app.get("/", (req, res) => {
+  return res.json("Hello World!");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post("/add", isValidNum, (req, res) => {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  } else if (num1 + num2 > 1000000) {
+    return res.json({
+      status: "error",
+      message: "Overflow",
+    });
+  } else
+    return res.json({
+      status: "success",
+      message: "the sum of given two numbers",
+      sum: num1 + num2,
+    });
+});
+
+app.post("/sub", isValidNum, (req, res) => {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  } else if (num1 - num2 < -1000000) {
+    return res.json({
+      status: `error`,
+      message: `Underflow`,
+    });
+  } else
+    return res.json({
+      status: "success",
+      message: "the difference of given two numbers",
+      difference: num1 - num2,
+    });
+});
+app.post("/multiply", isValidNum, (req, res) => {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  } else if (num1 * num2 > 1000000) {
+    return res.json({
+      status: "error",
+      message: "Overflow",
+    });
+  } else
+    return res.json({
+      status: "success",
+      message: "The product of given numbers",
+      result: num1 * num2,
+    });
+});
+app.post("/divide", isValidNum, (req, res) => {
+  const { num1, num2 } = req.body;
+
+  if (num2 === 0) {
+    return res.json({
+      status: `error`,
+      message: `Cannot divide by zero`,
+    });
+  }
+  const result = num1 / num2;
+
+  if (result > 1000000) {
+    return res.json({
+      status: `error`,
+      message: `Overflow`,
+    });
+  }
+
+  return res.json({
+    status: `success`,
+    message: `The division of given numbers`,
+    result,
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
